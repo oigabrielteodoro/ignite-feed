6bf39717d77248b12762142ae345c8661f74985b
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-08-26 12:29:04 -0300

    Add logo component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
+import Logo from "../Logo";
+
 import styles from "./Header.module.css";
 
 export default function Header() {
   return (
     <header className={styles.header}>
+      <Logo />
       <strong>Ignite Feed</strong>
     </header>
   );
