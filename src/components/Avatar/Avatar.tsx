ecc94b34c6e18cfc7eaa1bf38cc834d698ff4f44
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-08-26 18:04:38 -0300

    Add all image attributes in Avatar component

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,15 +1,17 @@
+import { ImgHTMLAttributes } from "react";
 import styles from "./Avatar.module.css";
 
 type Props = {
   src: string;
   hasBorder: boolean;
-};
+  alt?: string;
+} & ImgHTMLAttributes<HTMLImageElement>;
 
-export default function Avatar({ src, hasBorder }: Props) {
+export default function Avatar({ hasBorder, ...props }: Props) {
   return (
     <img
-      src={src}
       className={hasBorder ? styles.avatarWithBorder : styles.avatar}
+      {...props}
     />
   );
 }
